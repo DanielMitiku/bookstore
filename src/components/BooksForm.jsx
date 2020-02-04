719b2852156c63f4f77e6f0fc4b54ec59607eb8c
imhta <imhtapm@gmail.com>
2020-02-04 21:51:26 +0530

    :sparkles: create book form

---
+++ src/components/BooksForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function BooksForm() {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <form>
+      <input type="text" name="title" />
+      <select>
+        {categories.map(cat => (<option>{cat}</option>))}
+      </select>
+      <input type="sumbit" name="title" />
+    </form>
+  );
+}
+
+export default BooksForm;
