c33ba44fe0217f1049631d7998d4acae9ccecb7b
danielmitiku <wedanielmitiku@gmail.com>
2020-02-04 15:06:03 +0300

    added BooksList comoponent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
 
 function App() {
   return (
---
+++ src/containers/BooksList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function BooksList() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        <tr>
+          <td>id1</td>
+          <td>Title1</td>
+          <td>Category1</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default BooksList;
