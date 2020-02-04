1b4e3004de000a8bc8a3dfe809a0d4d3f4da62e5
danielmitiku <wedanielmitiku@gmail.com>
2020-02-04 15:09:14 +0300

    added Books Component

---
+++ src/components/Book.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Book = ({ book }) => {
+  <tr>
+    <td>{book.id}</td>
+    <td>{book.title}</td>
+    <td>{book.category}</td>
+  </tr>;
+};
+
+export default Book;
