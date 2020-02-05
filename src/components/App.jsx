e877b16e3de5768e94f13b252cdd8f3e69304a26
danielmitiku <wedanielmitiku@gmail.com>
2020-02-05 23:36:50 +0300

    added styling for navbar and filter select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,17 @@ import BooksForm from '../containers/BooksForm';
 function App() {
   return (
     <div>
-      <BooksList />
-      <BooksForm />
+      <nav className="nav">
+        <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+      </nav>
+      <div className="container">
+        <div className="booksList">
+          <BooksList />
+        </div>
+        <div className="booksForm">
+          <BooksForm />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 import categories from '../utils';
 
 const CategoryFilter = ({ onFilterChange }) => (
-  <select onChange={e => onFilterChange(e.target.value)}>
-    <option key="All" value="All">All</option>
-    {categories.map(cat => (
-      <option key={cat} value={cat}>{cat}</option>
-    ))}
-  </select>
+  <div className="filter">
+    <p>Filter By:
+      <select className="filterSelect" onChange={e => onFilterChange(e.target.value)}>
+        <option key="All" value="All">All</option>
+        {categories.map(cat => (
+          <option key={cat} value={cat}>{cat}</option>
+        ))}
+      </select>
+    </p>
+  </div>
 );
 
 CategoryFilter.propTypes = {
